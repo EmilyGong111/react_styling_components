6f9611ba7e51ea646b49b508e46fe9198a82d56c
Emily <mei.gong@icloud.com>
2022-05-27 16:30:45 +1000

    docs: add different way to add basic style in react

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,7 @@
 const Content = () => {
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'Dave'];
+        // get a random integer
         const int = Math.floor(Math.random() * 3);
         return names[int];
     }
@@ -10,6 +11,15 @@ const Content = () => {
             <p>
                 Hello {handleNameChange()}!
             </p>
+            {/* 
+            {}used for js expression
+            <p>html</p> -> html
+            <p>{"Emily"}</p> ->Emily
+            <p>{[1,2,3]}</p> ->123
+            <p>[1,2,3]</p> ->[1,2,3]
+            <p>{{a:1, b:2}}</p> ->error, cannot render an object
+            <p>{2===4}</p> ->empty, cannot render a boolean  
+            */}
         </main>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,22 @@
 const Header = () => {
-
+    // const headerStyle = {
+    //     backgroundColor:'pink',
+    //     color:'#000'
+    // }
     return (
         <header>
             <h1>Groceries List</h1>
         </header>
+
+        // inline style 
+        // <header style={{backgroundColor:'pink', color: '#000'}}>
+        //     <h1>Groceries List</h1>
+        // </header>
+
+        //inline style with variable
+        //  <header style={headerStyle}>
+        //     <h1>Groceries List</h1>
+        // </header>
     )
 }
 
